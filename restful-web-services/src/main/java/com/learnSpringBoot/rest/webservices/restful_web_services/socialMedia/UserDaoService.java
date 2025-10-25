a015c57100981885f53e2a9da4f3f0612625486a
Rajat Jain <rajatj611@gmail.com>
2025-10-25 23:48:55 +0530

    Added the user and userdao service

---
+++ restful-web-services/src/main/java/com/learnSpringBoot/rest/webservices/restful_web_services/socialMedia/UserDaoService.java
@@ -0,0 +1,25 @@
+package com.learnSpringBoot.rest.webservices.restful_web_services.socialMedia;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.ToDoubleBiFunction;
+
+@Component
+public class UserDaoService {
+    // TODO
+    // Replace the static lists with JPA/Hibernate/H2
+
+    private static final List<User> usersList = new ArrayList<>();
+
+    static{
+        usersList.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        usersList.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        usersList.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+    }
+    public List<User> findAllUsers(){
+        return usersList;
+    }
+}
